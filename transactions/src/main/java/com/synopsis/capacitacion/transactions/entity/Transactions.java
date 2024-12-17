bda84c7a81942b889c86573a655a77993a42946e
Boris Flores <bflorez@proveedor.intercorp.com.pe>
2024-12-16 19:28:34 -0500

    lunes

---
+++ transactions/src/main/java/com/synopsis/capacitacion/transactions/entity/Transactions.java
@@ -0,0 +1,25 @@
+package com.synopsis.capacitacion.transactions.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Transactions {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String reference;
+	private String accountIban;
+	private LocalDateTime date;
+	private Double amount;
+	private Double fee;
+	private String description;
+	private String status;
+	private String channel;
+}
