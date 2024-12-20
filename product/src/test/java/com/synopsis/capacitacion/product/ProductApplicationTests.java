1f7ce2ca87f05aaf9501c81ef22c67744cddb41f
Boris Flores <bflorez@proveedor.intercorp.com.pe>
2024-12-20 13:42:27 -0500

    final

--- product/src/test/java/com/synopsis/capacitacion/product/ProductApplicationTests.java
+++ product/src/test/java/com/synopsis/capacitacion/product/ProductApplicationTests.java
@@ -1,13 +1,13 @@
 package com.synopsis.capacitacion.product;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProductApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProductApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
