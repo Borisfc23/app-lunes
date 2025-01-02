38599a343b57e8bad843919495c90d425ab8dba5
Boris Flores <bflorez@proveedor.intercorp.com.pe>
2025-01-02 18:44:45 -0500

    update

--- alumnos/src/main/java/com/example/alumnos/service/AlumnoService.java
+++ alumnos/src/main/java/com/example/alumnos/service/AlumnoService.java
@@ -11,7 +11,8 @@ import com.example.alumnos.repository.AlumnoRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class AlumnoService {
@@ -23,17 +24,20 @@ public class AlumnoService {
 
 	@Autowired
 	private AlumnoRepository alumnosRepository;
+	
+	private static final Logger log = LoggerFactory.getLogger(AlumnoService.class);
 
 	private static final String EXTERNAL_API_URL = "https://jsonplaceholder.typicode.com/posts";
 
 	public void enviarYGuardarAlumno(AlumnoDTO request) {
-
+		log.info("Iniciando la tarea...");        
+		log.info("El nombre es ..."+request.getNombre());
+		log.info("El apellido es ..."+request.getApellido());
 		ExternalApiResponse response = restTemplate.postForObject(env.getProperty("endpoint.ms-alumnos.base-path"), request, ExternalApiResponse.class);
 
 		Alumnos alumno = new Alumnos();
 		alumno.setMensaje(response.getMensajeFormat());
-		alumno.setFechahora(LocalDateTime.now());
-
+		alumno.setFechahora(LocalDateTime.now());		
 		alumnosRepository.save(alumno);
 	}
 }
